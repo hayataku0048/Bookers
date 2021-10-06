aef9e11b20969e0a284925fd5ec7b7cc4e12b41f
hayataku48 <lintazai98@gmail.com>
2021-10-06 14:05:30 +0000

    finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,26 @@
 class BooksController < ApplicationController
-  def index
-  end
-
   def show
   end
 
+  def index
+  end
+
   def new
-    @list = List.new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path
   end
 
   def edit
   end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
